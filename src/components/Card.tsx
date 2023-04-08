7a646ed99f748f98aa57d7ceeee3a2f7646feacd
Krenedits Ferenc <fefe.kre@gmail.com>
2023-04-09 00:45:20 +0200

    challenge done

---
+++ src/components/Card.tsx
@@ -0,0 +1,47 @@
+import DividerDesktop from '../../images/pattern-divider-desktop.svg';
+import DividerMobile from '../../images/pattern-divider-mobile.svg';
+import Dice from '../../images/icon-dice.svg';
+
+interface CardProps {
+    number: number;
+    toggleRefresh: () => void;
+    children: React.ReactNode;
+}
+
+export default function Card({ children, number, toggleRefresh }: CardProps) {
+    const src = window.innerWidth < 500 ? DividerMobile : DividerDesktop;
+
+    return (
+        <div
+            style={{
+                backgroundColor: 'hsl(217, 19%, 24%)',
+                padding: '40px 20px',
+                paddingBottom: '80px',
+                borderRadius: '10px',
+                maxWidth: '500px',
+                position: 'relative',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '25px',
+                margin: '0 auto',
+            }}
+        >
+            <div
+                style={{
+                    color: 'hsl(150, 100%, 66%)',
+                    fontWeight: '800',
+                    fontSize: '12px',
+                    textTransform: 'uppercase',
+                    letterSpacing: '5px',
+                }}
+            >
+                Advice #{number || 0}
+            </div>
+            {children}
+            <img src={src} alt='divider' className='divider' />
+            <div className='dice' onClick={toggleRefresh}>
+                <img src={Dice} alt='dice' />
+            </div>
+        </div>
+    );
+}
